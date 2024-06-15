1b940e99a8aee8916958eec060e2b9d44308bb94
robpubking <mayblessingy@gmail.com>
2024-06-15 14:14:41 +0800

    add maximum sum sequence

---
+++ Project1/Max_Subsum_Sequence.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int max_sub(int arr[], int len);
+int result = 0;
+int main(void) 
+{
+
+	int arr[10] = { -4, -8, -5, -6, -12, -2, -1, -3, -5, -11 };
+
+	int n = max_sub(arr, 10);
+
+	cout << "n is : " << n << endl;
+
+	return 0;
+}
+
+
+int get_bigger(int a, int b) {
+
+	if (a > b) {
+	
+		return a;
+	}
+	else {
+	
+		return b;
+	}
+}
+
+int max_sub(int arr[], int len) {
+
+	vector<int> max_list;
+	int max_sum = arr[0];
+	int temp = 0;
+	bool temp_changed = false;
+	for (int i = 1; i < len; i++){
+
+		if (max_sum > 0)
+		{
+			if (arr[i] > 0)
+			{
+			 max_sum += arr[i];
+
+			}else if(max_sum + arr[i] >0){
+
+			 temp_changed = true;
+			 temp = get_bigger(temp, max_sum);
+			 max_sum += arr[i];
+					
+			}else {
+
+			 temp_changed = true;
+			 temp = get_bigger(temp, max_sum);
+			 max_sum = 0;
+			}
+
+		}else {
+		
+			max_sum = get_bigger(max_sum, arr[i]);
+		}
+		
+		}
+
+	   result = temp_changed ? get_bigger(max_sum, temp) : max_sum;
+
+	   return result;
+}
